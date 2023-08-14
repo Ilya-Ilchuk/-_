3c5ca53da9327959cd8d48488a16f3cd4d75592f
Illya Ilchuk <illya.illchuk@gmail.com>
2023-08-15 02:20:02 +0300

    somephing unusual

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+
+pygame.init()
+
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption('SSSSSSSSSSSSLLLLLLLCCCCCCCCCCCCC')
+fill_color = (32, 52, 71)
+rect_width, rect_height = 100, 200
+rect_x = screen_width / 2 - rect_width / 2
+rect_y = screen_height / 2 - rect_height / 2
+rect_color = pygame.Color("lightyellow")
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill(fill_color)
+    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
+    pygame.display.update()
