4ef52c573a57c46655cd740a5c4028ec0d4fd04c
Illya Ilchuk <illya.illchuk@gmail.com>
2023-08-15 02:57:31 +0300

    cube can move and can not out

--- main.py
+++ main.py
@@ -12,11 +12,24 @@ rect_width, rect_height = 100, 200
 rect_x = screen_width / 2 - rect_width / 2
 rect_y = screen_height / 2 - rect_height / 2
 rect_color = pygame.Color("lightyellow")
+
+STEP = 10
+
 while True:
     for event in pygame.event.get():
-        print(event)
+        # print(event)
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and rect_y > 0:
+                rect_y -= STEP
+            if event.key == pygame.K_DOWN and rect_y < screen_height - rect_height:
+                rect_y += STEP
+            if event.key == pygame.K_LEFT and rect_x > 0:
+                rect_x -= STEP
+            if event.key == pygame.K_RIGHT and rect_x < screen_width - rect_width:
+                rect_x += STEP
+                print(rect_x)
 
     screen.fill(fill_color)
     pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
